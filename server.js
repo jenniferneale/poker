f1a139825b328a21fa5eca563d0977a24b4110ff
jenniferneale <jeneale6@gmail.com>
2018-09-27 21:30:23 -0400

    add an express server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use('/node_modules', express.static('node_modules'));
+app.use('/', express.static('app'));
+app.listen(1337, () => console.log(
+    'Express server running at http://127.0.0.1:1337'));
