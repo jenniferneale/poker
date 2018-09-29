da0d5531a90c877e4d9583694a9f9c4c864ea6e1
jenniferneale <jeneale6@gmail.com>
2018-09-29 19:21:37 -0400

    separate out enums

---
+++ poker-game/constants.ts
@@ -0,0 +1,35 @@
+export enum Suit {
+    HEARTS,
+    SPADE,
+    DIAMOND,
+    CLUB
+}
+
+export enum Rank {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
+
+export enum HandRank {
+    HIGH_CARD,
+    PAIR,
+    DOUBLE_PAIR,
+    SET,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH,
+    ROYAL_FLUSH,
+}
