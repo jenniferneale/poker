4bd9b3157096346d42e51740678337ac42883c8d
jenniferneale <jeneale6@gmail.com>
2018-09-30 16:40:04 -0400

    add hand tests

--- test/poker.js
+++ test/poker.js
@@ -1,4 +1,5 @@
-import { Card } from '../public/build/card.js';
+import Card from '../public/build/card.js';
+import Hand from '../public/build/hand.js';
 import { Rank, Suit } from '../public/build/constants.js';
 import chai from 'chai';
 let should = chai.should();
@@ -20,4 +21,35 @@ describe('Cards', function() {
     done();
   });
  
+});
+
+describe('Hands', function() {
+
+    it('should make a new hand given a list of cards', function(done) {
+      let testHand = new Hand([
+        new Card(Rank.FIVE, Suit.CLUB),
+        new Card(Rank.ACE, Suit.HEART)
+      ]);
+      testHand.cards.length.should.equal(2);
+      testHand.toString().should.equal(
+        Rank[Rank.FIVE] + "-" + Suit[Suit.CLUB] + " " + 
+        Rank[Rank.ACE] + "-" + Suit[Suit.HEART]);
+      testHand.hasRank(Rank.ACE).should.be.true;
+      testHand.hasRank(Rank.THREE).should.be.false;
+      testHand.suits[Suit.HEART].length.should.equal(1);
+      testHand.suits[Suit.SPADE].length.should.equal(0);
+      done();
+    });
+
+    it('should make a new hand given a string list of cards', function(done) {
+      let testString = "SEVEN-DIAMOND SIX-HEART EIGHT-SPADE JACK-SPADE NINE-DIAMOND TEN-DIAMOND ACE-SPADE";
+      let testHand = Hand.fromString(testString);
+      testHand.cards.length.should.equal(7);
+      testHand.toString().length.should.equal(testString.length);
+      testHand.hasRank(Rank.ACE).should.be.true;
+      testHand.hasRank(Rank.THREE).should.be.false;
+      testHand.suits[Suit.DIAMOND].length.should.equal(3);
+      testHand.suits[Suit.CLUB].length.should.equal(0);
+      done();
+    })
 });
