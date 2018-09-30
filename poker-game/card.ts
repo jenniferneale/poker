73cb51b94ca88ceb1e8c803312416c97e6294da2
jenniferneale <jeneale6@gmail.com>
2018-09-30 01:27:45 -0400

    pull out card object to review

---
+++ poker-game/card.ts
@@ -0,0 +1,49 @@
+import { Rank, Suit } from './constants.js';
+
+export class Card {
+
+    /* Where possible, it's nice to have constants like suits and ranks defined in only one place. 
+    The arrays defined here previously have the advantage of being short-hand names, but if we really 
+    needed these, it would be preferable to include those as part of the suit and rank collection objects. */
+    private static suits: string[] = Object.keys(Suit).filter(k => typeof Suit[k as any] === "number");
+    private static ranks: string[] = Object.keys(Rank).filter(k => typeof Rank[k as any] === "number");
+    private static delim: string = "-";
+
+    constructor(private _rank: Rank, private _suit: Suit) {
+        if (this._rank >= Card.ranks.length)
+            throw Error('A rank must be 0 and 12. ( Ace to king)');
+        if (this._suit >= Card.suits.length)
+            throw Error('A suit must be between 0 and 3 (h, s, d, c)')
+    }
+
+    toString() {
+        return Card.ranks[this._rank] + Card.delim + Card.suits[this._suit];
+    }
+
+    static sortFn(a: Card, b: Card) {
+        return b.rank - a.rank;
+    }
+
+    get suit() {
+        return this._suit;
+    }
+
+    get rank() {
+        return this._rank;
+    }
+
+    // transform EIGHT-CLUB type of strings to a new Card
+    static fromString(str: string) {
+        let splits = str.split(Card.delim);
+        if (splits.length !== 2)
+            throw Error('Card length should be 2 words, delimited by -');
+        
+        const rank = Card.ranks.findIndex(x => x === splits[0]);
+        const suit = Card.suits.findIndex(x => x === splits[1]);
+
+        if (rank === -1 || suit === -1)
+            throw Error(`Rank ${str[0]} or suit ${str[1]} was not found`);
+
+        return new Card(rank, suit);
+    }
+}
--- poker-game/poker.ts
+++ poker-game/poker.ts
@@ -1,47 +1,6 @@
 import { Suit, Rank, HandRank } from './constants.js';
+import { Card } from './card.js';
 
-export class Card {
-
-    private static suits: string[] = [ 'h', 's', 'd', 'c' ];
-    private static ranks: string[] = [ '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' ];
-
-    constructor(private _rank: Rank, private _suit: Suit) {
-        if (this._rank >= Card.ranks.length)
-            throw Error('A rank must be 0 and 12. ( Ace to king)');
-        if (this._suit >= Card.suits.length)
-            throw Error('A suit must be between 0 and 3 (h, s, d, c)')
-    }
-
-    toString() {
-        return Card.ranks[this._rank] + Card.suits[this._suit];
-    }
-
-    static sortFn(a: Card, b: Card) {
-        return b.rank - a.rank;
-    }
-
-    get suit() {
-        return this._suit;
-    }
-
-    get rank() {
-        return this._rank;
-    }
-
-    // transfor 4d, 7s type of strings to a new Card
-    static fromString(str: string) {
-        if (str.length !== 2)
-            throw Error('Card length should be 2');
-
-        const rank = Card.ranks.findIndex(x => x === str[0]);
-        const suit = Card.suits.findIndex(x => x === str[1]);
-
-        if (rank === -1 || suit === -1)
-            throw Error(`Rank ${str[0]} or suit ${str[1]} was not found`);
-
-        return new Card(rank, suit);
-    }
-}
 /*
 // the Hand class contains the player cards + board cards.
 export class Hand {
@@ -267,4 +226,5 @@ export class WinningHand extends Hand{
 }*/
 
 let testCard = new Card(Rank.EIGHT, Suit.CLUB);
-console.log("test2" + Suit.CLUB + "-" + testCard);
+
+console.log("test2" + Suit.CLUB + "-" + testCard + "  " + Object.keys(Rank) + "  " + Card.fromString("FOUR-HEARTS") + " " + (typeof testCard.rank == typeof Rank.EIGHT));
