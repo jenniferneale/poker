145b897ca3ab458b9576d91ddd306632a93c6c9b
jenniferneale <jeneale6@gmail.com>
2018-09-30 16:39:09 -0400

    comments on hand object

--- poker-game/hand.ts
+++ poker-game/hand.ts
@@ -1,11 +1,12 @@
-import { Suit, Rank, HandRank } from './constants.js';
-import { Card } from './card.js';
+import { Rank } from './constants.js';
+import Card from './card.js';
 
 // the Hand class contains the player cards + board cards.
-export class Hand {
+export default class Hand {
     public cards: Card[] = [];
     // those two are used when evaluating a hand
     // take a look at evaluate hand for more details
+    // 2-dimensional arrays to list how many of each kind we have
     public suits: Card[][] = new Array(4).fill(0).map(_ => []);
     public ranks: Card[][] = new Array(13).fill(0).map(_ => []);
 
@@ -16,7 +17,7 @@ export class Hand {
     addCard(card: Card): Hand {
         if (this.cards.length > 7)
             throw Error('Hand containing board card must have max 7 cards. This is Hold\'em');
-        // precautious check to see that we can actually add the card (there shouldn't be two Ad in a deck for example)
+        // cautious check to see that we can actually add the card (there shouldn't be two ACE-DIAMOND in a deck for example)
         if (this.cards.some(c => c.toString() === card.toString()))
             throw Error('This card has already been added to this hand!');
 
@@ -34,13 +35,17 @@ export class Hand {
         if (this.cards.length < 7)
             throw new Error('When evaluating a hand, the hand must have 7 cards');
 
+        // TODO: MISSING EVALUATION FUNCTION
+
     }
 
+    // Check if hand contains one or more of the given Rank
     hasRank(rank: Rank) {
         return this.ranks[rank].length > 0;
     }
 
-    // gives back a string representation of the hand of the form: 7d 6h 8s Js 9s Td As
+    // gives back a string representation of the hand of the form: 
+    // SEVEN-DIAMOND SIX-HEART EIGHT-SPADE JACK-SPADE NINE-DIAMOND TEN-DIAMOND ACE-SPADE
     toString() {
         return this.cards.toString().replace(/,/g, ' ');
     }
