7b872e328bb92c0482f0ab0c5d3422fe1e66aa27
jenniferneale <jeneale6@gmail.com>
2018-10-01 01:22:17 -0400

    comments on HandRank

--- poker-game/constants.ts
+++ poker-game/constants.ts
@@ -25,11 +25,14 @@ export enum HandRank {
     HIGH_CARD,
     PAIR,
     DOUBLE_PAIR,
-    SET,
+    SET, // Also called three of a kind
     STRAIGHT,
     FLUSH,
     FULL_HOUSE,
     FOUR_OF_A_KIND,
     STRAIGHT_FLUSH,
-    ROYAL_FLUSH,
+    ROYAL_FLUSH, 
+    /* Having a separate Royal flush is probably not necessary, 
+    since it's the highest straight flush, but it's probably nice for 
+    labeling purposes in an expanded version of this. */
 }
