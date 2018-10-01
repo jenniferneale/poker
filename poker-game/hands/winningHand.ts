6968ec57f6dfb9e54c65ed8cbdf49ed60462b61e
jenniferneale <jeneale6@gmail.com>
2018-10-01 01:25:15 -0400

    Comments on winningHand instead of filling in TODO

--- poker-game/hands/winningHand.ts
+++ poker-game/hands/winningHand.ts
@@ -8,7 +8,7 @@ import { HandRank } from '../constants.js';
 export default class WinningHand extends Hand{
 
     constructor(cards: Card[], public rank: HandRank) {
-        
+
         // passed in cards to the super, instead of setting cards after
 
         super(cards);
@@ -17,9 +17,16 @@ export default class WinningHand extends Hand{
     // If a hand has a rank of PAIR
     // we need to be able to compare it with another
     // winning hand that is also pair. Thus we need additional information,
-    // like the high card, etc.
-    // We will total 6 ranks
+    // like the high card, etc. -----> Double Pair: Compare Pair1 rank, compare Pair2 rank, compare fifth card
+    //                                 Pair: Compare Pair1 rank, compare highest card, compare next highest card, compare last card
+    // We will total 6 ranks --------> I'm not sure what this means. Are there 6 players we're comparing?
     calculateRanks(rank: HandRank){
-        // TODO
+        /* Pairs are not the only hands that require additional information in event of a similar HandRank. 
+            You could write additional comparison functions for each case, like the HandEvaluator, but that sounds dreadful. 
+            A branching system or a point system might be a better bet, if that holds up. 
+            Eg, x points for a flush, y points for a straight, points for k of a kind. 
+            There are technically a finite number of card combinations, but it's more than you'd want to store 
+            without a clever way to do it: 2,598,960, according to https://www.chemical-ecology.net/java/possible.htm
+            */
     }
 }
